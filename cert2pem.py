9516dbadf62a53d3028d7250236ae7b4128c9899
funzoneq <arnoud@tumblr.com>
2013-02-28 11:09:39 +0100

    Get the subjects

--- cert2pem.py
+++ cert2pem.py
@@ -1,6 +1,7 @@
 import re
 import textwrap
 import base64
+import M2Crypto
 from pprint import pprint
 
 with open('certdata.txt') as f:
@@ -9,11 +10,16 @@ with open('certdata.txt') as f:
 
 	pat = re.compile(r"^CKA_VALUE MULTILINE_OCTAL\n(.*?)END\n\n", re.S|re.M)
 	result = pat.findall(data)
-	value = ""
 	for i in result:
+		value = ""
 		line = "".join(i.split("\n"))
 		for a in re.finditer(r'\\([0-3][0-7][0-7])', line):
 			value += chr(int(a.group(1), 8))
-		print "-----BEGIN CERTIFICATE-----"
-		print "\n".join(textwrap.wrap(base64.b64encode(value), 64))
-		print "-----END CERTIFICATE-----"
+		pem = "-----BEGIN CERTIFICATE-----\n%s\n-----END CERTIFICATE-----" % "\n".join(textwrap.wrap(base64.b64encode(value), 64))
+		cert = M2Crypto.X509.load_cert_string(str(pem))
+		try:
+			subject = cert.get_subject().as_text()
+			print subject
+		except:
+			print "issue/subject: Unexpected error:", sys.exc_info()[0]
+			pass
